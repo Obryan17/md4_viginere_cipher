faed5663bde8998b0538b40c212bf3c490c24bee
Obryan17 <111773005+Obryan17@users.noreply.github.com>
2025-12-02 22:56:06 +0100

    feature : cesar uncipher function

---
+++ main.py
@@ -0,0 +1,19 @@
+message="lapin"
+key = 3
+def cesar_cipher(message , key):
+    crypted_message="" 
+    for char in message:
+        crypted_char = chr((ord(char)+key) % 1_114_112)
+        crypted_message += crypted_char
+    return crypted_message
+
+
+def cesar_uncipher(crypted_message , key ):
+    return cesar_cipher( crypted_message , -key )
+
+
+crypted_message=cesar_cipher(message="lapin" , key=3)
+print(crypted_message)
+
+initial_message = cesar_uncipher(crypted_message=crypted_message , key=3)
+print(initial_message)
