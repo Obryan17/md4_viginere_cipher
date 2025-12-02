dd4996f7cb21c22b5c9b915320c6cee0b9943485
Obryan17 <111773005+Obryan17@users.noreply.github.com>
2025-12-02 23:07:47 +0100

    feature : brute force cesar cipher function

--- main.py
+++ main.py
@@ -1,3 +1,5 @@
+import string
+
 message="lapin"
 key = 3
 def cesar_cipher(message , key):
@@ -12,8 +14,19 @@ def cesar_uncipher(crypted_message , key ):
     return cesar_cipher( crypted_message , -key )
 
 
-crypted_message=cesar_cipher(message="lapin" , key=3)
+def brute_force_cesar_cipher (crypted_message):
+    for potential_key in range(1 , 1_114_112):
+        potential_message = cesar_uncipher(crypted_message , key=potential_key)
+        if potential_message[0] in string.ascii_letters:
+            print(potential_key)
+            print(potential_message)
+            print("--------")
+
+
+crypted_message=cesar_cipher(message="lapin" , key=554)
 print(crypted_message)
 
-initial_message = cesar_uncipher(crypted_message=crypted_message , key=3)
+initial_message = cesar_uncipher(crypted_message=crypted_message , key=554)
 print(initial_message)
+
+brute_force_cesar_cipher(crypted_message)
